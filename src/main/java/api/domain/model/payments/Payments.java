042f064d609cac22da7be77efd2d129eceed74a4
masahiro-ikeda <masahiro.ikeda@careritz.co.jp>
2021-08-02 00:23:21 +0900

    支払合計金額モデルをフィールド変数から除去

--- src/main/java/api/domain/model/payments/Payments.java
+++ src/main/java/api/domain/model/payments/Payments.java
@@ -11,13 +11,13 @@ import java.util.List;
  * 支払記録の集約.
  */
 public class Payments {
+  // 支払記録一覧
   private final List<Payment> paymentList;
-  private PaymentTotalAmount paymentTotalAmount;
 
   /**
    * コンストラクタ.
    *
-   * @param paymentList
+   * @param paymentList 支払記録一覧
    */
   public Payments(List<Payment> paymentList) {
 
@@ -25,25 +25,19 @@ public class Payments {
       throw new IllegalArgumentException( "Payments Not Permit Null." );
     }
     this.paymentList = paymentList;
-    this.paymentTotalAmount = new PaymentTotalAmount( paymentList );
   }
 
   /**
    * 支払.
    *
-   * @param yenCurrency
-   * @return PaymentTotalAmount 支払金額合計
+   * @param yenCurrency 支払った貨幣
    */
-  public PaymentTotalAmount pay(YenCurrency yenCurrency) {
+  public void pay(YenCurrency yenCurrency) {
 
     // 新規支払インスタンスの生成
     Payment newPayment = PaymentFactory.newPay( yenCurrency );
     // リスト追加
     paymentList.add( newPayment );
-    // 支払合計の再計算
-    this.paymentTotalAmount = new PaymentTotalAmount( paymentList );
-
-    return this.paymentTotalAmount;
   }
 
   /**
@@ -53,24 +47,20 @@ public class Payments {
     // 購入記録の生成
     Payment purchasePayment = PaymentFactory.newPurchasePay( drinkPrice );
     paymentList.add( purchasePayment );
-    // 支払合計の再計算
-    this.paymentTotalAmount = new PaymentTotalAmount( paymentList );
   }
 
   /**
    * 返金.
    */
   public ReturnAmount repay() {
-    // 返金額インスタンスの生成
-    ReturnAmount returnAmount = new ReturnAmount( this.paymentTotalAmount.value() );
+    // 返金前の支払金額合計
+    var paymentTotalAmountBeforeRepay = new PaymentTotalAmount( paymentList );
 
     // 返金記録の作成
-    Payment repayment = PaymentFactory.newRepay( this.paymentTotalAmount );
+    Payment repayment = PaymentFactory.newRepay( paymentTotalAmountBeforeRepay );
     paymentList.add( repayment );
-    // 支払合計の再計算
-    this.paymentTotalAmount = new PaymentTotalAmount( paymentList );
 
-    return returnAmount;
+    return new ReturnAmount( paymentTotalAmountBeforeRepay.value() );
   }
 
   /**
@@ -79,22 +69,21 @@ public class Payments {
    * @return ReturnAmount おつり金額
    */
   public ReturnAmount change() {
-    ReturnAmount returnAmount = new ReturnAmount( paymentTotalAmount.value() );
+    // おつり返却前の支払金額合計
+    var paymentTotalAmountBeforeChange = new PaymentTotalAmount( paymentList );
 
     // お釣り記録の生成
-    Payment changePayment = PaymentFactory.newChange( paymentTotalAmount );
+    Payment changePayment = PaymentFactory.newChange( paymentTotalAmountBeforeChange );
     paymentList.add( changePayment );
-    // 支払合計の再計算
-    this.paymentTotalAmount = new PaymentTotalAmount( paymentList );
 
-    return returnAmount;
+    return new ReturnAmount( paymentTotalAmountBeforeChange.value() );
   }
 
   /**
    * @return PaymentTotalAmount 支払金額合計
    */
   public PaymentTotalAmount paymentTotalAmount() {
-    return paymentTotalAmount;
+    return new PaymentTotalAmount( paymentList );
   }
 
   /**
