a4ef6e0b1a257a6be53117294c48a5c92df9a0ad
masahiro-ikeda <masahiro.ikeda@careritz.co.jp>
2023-10-28 15:26:46 +0900

    不要ファイルの削除など

--- src/test/java/api/application/payment/PaymentUsecaseTest.java
+++ src/test/java/api/application/payment/PaymentUsecaseTest.java
@@ -1,6 +1,5 @@
 package api.application.payment;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
